bf6bb7c1c5f131fc4684bb84fa1389494423f993
Matt Enyeart <menyeart1@gmail.com>
2023-04-07 09:49:39 -0600

    Create foods facade

---
+++ app/facades/foods_facade.rb
@@ -0,0 +1,26 @@
+class FoodsFacade
+  def initialize
+  end
+
+  def total_items(food)
+    FoodService.food_search(food)[:totalHits]
+  end
+
+  def food_search(food)
+    FoodService.food_search(food)[:foods].map do |food|
+      Food.new(get_info(food))
+    end
+  end
+
+  def get_info(food)
+    attributes = Hash.new(nil)
+    attributes[:code] = food[:gtinUpc]
+    attributes[:description] = food[:description]
+    attributes[:brand_owner] = food[:brandOwner]
+    attributes[:ingredients] = food[:ingredients]
+  end
+
+
+
+
+end
